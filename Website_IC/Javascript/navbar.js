19ecd0c5830234847fb6f875a89381ca6c11d390
TijsDeBelie <Tijsdebelie1997@gmail.com>
2016-11-28 20:01:06 +0100

    Finale versie van de site

--- Website_IC/Javascript/navbar.js
+++ Website_IC/Javascript/navbar.js
@@ -1,13 +1,9 @@
-Function Navbar(){
-    $(document).ready(function() { 
-    $('nav ul li a:not(:only-child)').click(function(e){
-        $(this).siblings('.submenu').slideToggle('slow');
-    });
-    
-    $('.nav-mobile').click(function(){
-        $('.nav-1 ul:not(.submenu)').slideToggle();
-        $(".line").toggleClass("active");
-    });
-
-	});
-		}
+$(document).ready(function () {
+			$('nav ul li a:not(:only-child)').click(function (e) {
+				$(this).siblings('.submenu').slideToggle('slow');
+			});
+			$('.nav-mobile').click(function () {
+				$('.nav-1 ul:not(.submenu)').slideToggle();
+				$(".line").toggleClass("active");
+			});
+		});
